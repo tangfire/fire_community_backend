2299bc6d1201a84f0fb610bb149d3f5d75166a73
tangfire <52628216@qq.com>
2024-11-30 00:13:34 +0800

    Initial commit

---
+++ src/main/java/com/example/fire_community_backend/FireCommunityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fire_community_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireCommunityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FireCommunityBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fire_community_backend/FireCommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fire_community_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireCommunityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
